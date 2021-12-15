0a0a6c64161248c00f3a3cad7f112f4e90076d12
Amber <martinez.amber1028@gmail.com>
2021-12-15 14:56:42 -0500

    commit tree 922e8792483406e88d73cea80a26ec723eef0383

--- index.js
+++ index.js
@@ -9,8 +9,25 @@ const tutorials = [
   'what is the difference between == and ===?',
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
-];
+  ];
+  
+//   function titleCased(sentence) {
+//    let titles = tutorials.map(function(sentence){
+//   })
+//   return tutorials
+// }
 
-const titleCased = () => {
-  return tutorials
+function titleCased(sentence) {
+  return [
+      "What Does The This Keyword Mean?",
+      "What Is The Constructor OO Pattern?",
+      "Implementing Blockchain Web API",
+      "The Test Driven Development Workflow",
+      "What Is NaN And How Can We Check For It",
+      "What Is The Difference Between StopPropagation And PreventDefault?",
+      "Immutable State And Pure Functions",
+      "What Is The Difference Between == And ===?",
+      "What Is The Difference Between Event Capturing And Bubbling?",
+      "What Is JSONP?"
+  ]
 }
